09e162aba1771a3efb1996bc99de2a2c7ab8d52b
aridoza <aridoza85@gmail.com>
2017-03-13 22:15:37 -0400

    animsition fade-up added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const arrowImg = document.querySelector('.down-arrow');
 const billContent = document.querySelector('.history');
+
+$(".animsition").animsition({
+  inClass: 'fade-in-up',
+  outClass: 'fade-out-up',
+  linkElement: '.bill-intro-div',
+  inDuration: 1000,
+  outDuration: 500
+});
